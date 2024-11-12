464d7ead2203d70f1392bd357afe18b622948ecf
Jackson <jacksyntax20@gmail.com>
2024-11-12 11:05:44 -0300

    fix: test checkInDate

---
+++ src/use-cases/check-in.ts
@@ -0,0 +1,38 @@
+import { CheckIn } from '@prisma/client'
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+
+interface CheckInUseCaseRequest {
+  userId: string
+  gymId: string
+}
+
+interface CheckInUseCaseResponse {
+  checkIn: CheckIn
+}
+
+export class CheckInUseCase {
+  constructor(private checkInsRespository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    gymId,
+  }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const checkInOnSameDay = await this.checkInsRespository.findByUserIdOnDate(
+      userId,
+      new Date()
+    )
+
+    if (checkInOnSameDay) {
+      throw new Error()
+    }
+
+    const checkIn = await this.checkInsRespository.create({
+      gym_id: gymId,
+      user_id: userId,
+    })
+
+    return {
+      checkIn,
+    }
+  }
+}
